dc087abf8ab6e4ee15859629448657d525521299
oscarvx00 <oscarvicente@usal.es>
2022-08-15 21:58:09 +0200

    Ia and scrapping

--- ia-training/main.py
+++ ia-training/main.py
@@ -3,6 +3,7 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 
@@ -22,7 +23,25 @@ x = StandardScaler().fit_transform(x)
 
 pca = PCA(n_components=2)
 principalComponents = pca.fit_transform(x)
-principalDf = pd.DataFrame(data = principalComponents, columns=['pc1', 'pc2'])
+principalDf = pd.DataFrame(data=principalComponents, columns=['pc1', 'pc2'])
 
 finalDf = pd.concat([principalDf, df[['target']]], axis=1)
 
+fig = plt.figure(figsize = (8,8))
+ax = fig.add_subplot(1,1,1)
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_title('2 component PCA', fontsize = 20)
+targets = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+colors = ['r', 'g', 'b']
+for target, color in zip(targets,colors):
+    indicesToKeep = finalDf['target'] == target
+    ax.scatter(finalDf.loc[indicesToKeep, 'pc1']
+               , finalDf.loc[indicesToKeep, 'pc2']
+               , c = color
+               , s = 50)
+ax.legend(targets)
+ax.grid()
+print('vamoss')
+plt.savefig('/tmp/figs/fig.png')
+plt.show()
