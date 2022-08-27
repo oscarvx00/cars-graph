500e3c64fe4fa70ddb25d5de3760d49577499a17
oscarvx00 <oscarvicente@usal.es>
2022-08-27 14:13:32 +0200

    Updated data classifier script

--- Jenkinsfile
+++ Jenkinsfile
@@ -12,12 +12,6 @@ pipeline  {
                     stash includes: 'model.zip', name: 'model'
                 }
             }
-            /*
-            post {
-                success {
-                    archiveArtifacts 'ia-classifier-train/model.zip'
-                }
-            }*/
         }
         stage('Generate ia workspace'){
             agent any
@@ -25,6 +19,7 @@ pipeline  {
                 dir('ia-classifier'){
                     unstash 'model'
                     sh 'unzip -o model.zip'
+                    sh 'rm model.zip'
                     sh 'zip -r ia-classifier.zip ./* '
                 }
             }
--- ia-classifier/data-classifier.py
+++ ia-classifier/data-classifier.py
@@ -1,27 +1,54 @@
-from cProfile import label
 import tensorflow as tf
 import numpy as np
-import pandas as pd
 from tensorflow import keras
+import argparse
+import csv
 
+parser = argparse.ArgumentParser(description='Cars predictor')
+parser.add_argument('--source', type=str, required=True, help='csv file path with data source')
+
+args = parser.parse_args()
+
+source_data = []
+#Load csv data
+with open(args.source) as source_file:
+    reader = csv.DictReader(source_file)
+    for data in reader:
+        source_data.append(data)
+    
+if len(source_data) == 0:
+    print('[ERROR] Source file has no data')  
+
+#Parse integer values
+integer_header = ["year", "kms", "seats", "power", "doors", "price"]
+for item in source_data:
+    for header in integer_header:
+        item[header] = int(item[header])
+
+
+#Load keras model
 model = tf.keras.models.load_model('model')
 
+keys = ["brand", "year", "kms", "city", "seats", "power", "transmission", "fuel", "doors", "price"]
 
-sample = {
-    "brand" : "MERDECES-BENZ",
-    "year" : 2002,
-    "kms" : 200000,
-    "city" : "Alicante",
-    "seats" : 4,
-    "power" : 218,
-    "transmission" : "Automático",
-    "fuel" : "Gasolina",
-    "doors" : 2,
-    "price" : 5000
-}
+predicted_data = []
 
-input_dict = {name: tf.convert_to_tensor([value]) for name, value in sample.items()}
-predictions = model.predict(input_dict)
+#Predict for all values, then push to array
+for index, car in enumerate(source_data):
+    sample = {key: car[key] for key in keys}
+    model_dict = {name: tf.convert_to_tensor([value]) for name, value in sample.items()}
+    predictions = model.predict(model_dict)
+    pred = np.argmax(predictions)
+    sample['rating'] = pred
+    sample['url'] = source_data[index]['url']
+    predicted_data.append(sample)
 
-pred = np.argmax(predictions)
-print("Coche cat " + str(pred))
+
+#Order by rating
+predicted_data = sorted(predicted_data, key=lambda d: d['rating'], reverse=True)
+
+#Save predictions to csv
+with open('predictions.csv', 'w') as output:
+    w = csv.DictWriter(output, predicted_data[0].keys())
+    w.writeheader()
+    w.writerows(predicted_data)
